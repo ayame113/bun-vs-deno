1d941d458ee9cd67fe968b83d6cfbcaa543a7150
ayame113 <40050810+ayame113@users.noreply.github.com>
2022-09-21 22:05:59 +0900

    static file serve

--- deno.ts
+++ deno.ts
@@ -1,1 +1,5 @@
-Deno.serve(() => new Response("Hello world!"), { port: 8000 });
+// Deno.serve(() => new Response("Hello world!"), { port: 8000 });
+Deno.serve(
+  async () => new Response((await Deno.open("./index.html")).readable),
+  { port: 8000 },
+);
