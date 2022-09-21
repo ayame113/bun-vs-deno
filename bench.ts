540af0a377fd1540609aa85505c4823471ff1171
ayame113 <40050810+ayame113@users.noreply.github.com>
2022-09-21 21:16:53 +0900

    tyr

--- bench.ts
+++ bench.ts
@@ -3,15 +3,15 @@ const commands = [
 ];
 
 for (const command of commands) {
-  const benchmark = Deno.spawn("./oha-linux-amd64", {
-    args: ["-j", "--no-tui", "http://localhost:9000/"],
-    stdout: "inherit",
-    stderr: "inherit",
-  });
   const server = new AbortController();
   Deno.spawn(command[0], {
     args: command.slice(1),
     signal: server.signal,
+  });
+  const benchmark = Deno.spawn("./oha-linux-amd64", {
+    args: ["-j", "--no-tui", "http://localhost:9000/"],
+    stdout: "inherit",
+    stderr: "inherit",
   });
   await benchmark.then((bench) => {
     console.log(bench.success);
