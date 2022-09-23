1c1470a52e7f223e254ff08d235d34b42bd19740
ayame113 <40050810+ayame113@users.noreply.github.com>
2022-09-23 19:24:11 +0900

    test short string

--- bun.js
+++ bun.js
@@ -1,7 +1,7 @@
 export default {
   port: 8000,
   fetch() {
-    // return new Response("Hello world!");
-    return new Response(Bun.file("./index.html"));
+    return new Response("Hello world!");
+    // return new Response(Bun.file("./index.html"));
   },
 };
