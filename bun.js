7a3770af465007263898b940ed8ad73eef87277d
ayame113 <40050810+ayame113@users.noreply.github.com>
2022-09-23 20:01:49 +0900

    bun date header spec

--- bun.js
+++ bun.js
@@ -1,7 +1,9 @@
 export default {
   port: 8000,
   fetch() {
-    return new Response("Hello world!");
+    return new Response("Hello world!", {
+      headers: { "Date": new Date().toUTCString() },
+    });
     // return new Response(Bun.file("./index.html"));
   },
 };
