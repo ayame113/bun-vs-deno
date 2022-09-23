8a163b8337cc09462c0e7db30669120ce2fe6d34
ayame113 <40050810+ayame113@users.noreply.github.com>
2022-09-23 20:25:52 +0900

    test rand

--- bun.js
+++ bun.js
@@ -1,7 +1,7 @@
 export default {
   port: 8000,
   fetch() {
-    return new Response("Hello world!", {
+    return new Response("Hello world!" + Math.random(), {
       headers: { "Date": new Date().toUTCString() },
     });
     // return new Response(Bun.file("./index.html"));
--- deno.js
+++ deno.js
@@ -1,4 +1,4 @@
-Deno.serve(() => new Response("Hello world!"), { port: 8000 });
+Deno.serve(() => new Response("Hello world!" + Math.random()), { port: 8000 });
 // Deno.serve(
 //   async () => new Response((await Deno.open("./index.html")).readable),
 //   { port: 8000 },
