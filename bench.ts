b62b42ae97942d1fbdbe01ab8e3a76ea4e7548f4
ayame113 <40050810+ayame113@users.noreply.github.com>
2022-09-23 23:15:23 +0900

    uuuuuuu

--- bench.ts
+++ bench.ts
@@ -4,24 +4,24 @@ const commands = [
   ["bun", "run", "bun.js"],
 ];
 
-// const bench = [
-//   "./oha-linux-amd64",
-//   "-j",
-//   "-n",
-//   "10000",
-//   "--no-tui",
-//   "http://127.0.0.1:4544/",
-// ];
 const bench = [
-  "wrk",
-  // "-t12",
-  // "-c",
-  // "400",
-  "-d",
-  "20s",
-  "--latency",
+  "./oha-linux-amd64",
+  "-j",
+  "-n",
+  "10000",
+  "--no-tui",
   "http://127.0.0.1:4544/",
 ];
+// const bench = [
+//   "wrk",
+//   // "-t12",
+//   // "-c",
+//   // "400",
+//   "-d",
+//   "20s",
+//   "--latency",
+//   "http://127.0.0.1:4544/",
+// ];
 
 for (const command of commands) {
   console.log("=".repeat(30));
