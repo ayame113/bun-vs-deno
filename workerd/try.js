ecc0a454f4d901b11e7e293b3619adcaa05cf059
ayame113 <40050810+ayame113@users.noreply.github.com>
2022-09-28 01:06:12 +0900

    work?

--- workerd/try.js
+++ workerd/try.js
@@ -1,7 +1,7 @@
 console.log("hello from workerd");
 
 console.log(globalThis);
-addEventListener("fetch", (event) => {
-  event.respondWith(handle(event.request));
-});
-throw new Error("haha");
+// addEventListener("fetch", (event) => {
+//   event.respondWith(handle(event.request));
+// });
+// throw new Error("haha");
